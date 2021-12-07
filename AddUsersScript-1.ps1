d49cc96a6c7f216bc803585c69838f1f3f0f8724
Charlie Dolton <71903635+charliedolton@users.noreply.github.com>
2021-12-06 16:29:15 -0800

    Add files via upload

---
+++ AddUsersScript-1.ps1
@@ -0,0 +1,163 @@
+﻿. C:\Scripts\Var_Definition.ps1
+
+$CSVFile = $args[0]
+$CSVFilePath = $ScriptsFilePath + "\" + $CSVFile
+$List = Import-Csv $CSVFilePath
+$UniqueUserList = $List | Select-Object FirstName,LastName,'Student ID',DOB -Unique
+$UniqueCourseList = $List | Select-Object Class -Unique
+
+
+#This loop creates the class groups
+foreach ($line in $UniqueCourseList)
+{
+    $ClassGRP = $line.Class
+
+    try {
+        New-ADGroup -GroupScope Global -Name $ClassGRP -Path $OUPathForGroups
+        "$Time $ClassGRP was added to Groups" | Out-File $ScriptsFilePath\groupCreation.log -Append
+    } catch {
+        $ErrorMessage = $_.Exception.Message
+        $FailedItem = $_.Exception.ItemName
+        "$Time Failed to create group $ClassGRP $ErrorMessage" | Out-File $ScriptsFilePath\groupCreation.log -Append
+        }
+}
+
+#This loop creates the unique user accounts
+foreach ($line in $UniqueUserList)
+{
+    $FirstName = $line.firstname
+    $FirstNameLetter = $line.Firstname[0]
+    $LastName = $line.lastname
+    $SID = $line.'Student ID'
+
+    $fullName = $SID + $FirstNameLetter + $LastName
+
+    $EncPass = (ConvertTo-SecureString -AsPlainText $line.DOB -Force)
+
+    $HomeDirectoryLocation = $HomeDirectory + $fullName
+
+    if (!(Test-Path -Path $HomeDirectoryLocation)) {
+        New-Item -ItemType Directory -Path $HomeDirectory -Name $fullName
+        "$Time Home directory created for $fullName" | Out-File $ScriptsFilePath\userCreation.log -Append
+    } else {
+        "$Time Home directory for $fullName already exists" | Out-File $ScriptsFilePath\userCreation.log -Append
+        }
+   
+    $HomeDirectoryLocation = $HomeDirectory + $fullName
+    
+    try {
+        New-ADUser -Name $fullName -GivenName $FirstName -Surname $LastName -Path $OUPathForAccounts -AccountPassword $EncPass -HomeDirectory $HomeDirectoryLocation -PostalCode 0
+        Set-ADUser -Identity $fullName
+        "$Time User $fullName was created successfully." | Out-File $ScriptsFilePath\userCreation.log -Append
+    } catch {
+        $ErrorMessage = $_.Exception.Message
+        $FailedItem = $_.Exception.ItemName
+        "$Time Failed to create user $fullName $ErrorMessage" | Out-File $ScriptsFilePath\userCreation.log -Append
+    }
+}
+
+#loop to add and remove students from class groups
+foreach ($line in $List)
+{
+
+    $FirstName = $line.firstname
+    $FirstNameLetter = $line.Firstname[0]
+    $LastName = $line.lastname
+    $SID = $line.'Student ID'
+
+    $fullName = $SID + $FirstNameLetter + $LastName
+
+    $class = $line.Class
+    $status = $line.Status
+    $currentUserClasses = Get-ADPrincipalGroupMembership -Identity $fullName | select name
+    $checkClass = "*"+$class+"*"
+    
+    if ($currentUserClasses -like $checkClass)
+    {
+        "$Time $fullName already enrolled in $class" | Out-File $ScriptsFilePath\groupAssignment.log -Append
+        continue
+    }
+
+    if ($status -eq "R" -or $status -eq "A")
+    {   
+        try {
+            $currentUser = Get-ADUser -Identity $fullName -Properties postalCode
+            $currentGroup = Get-ADGroup -Identity $class
+            Add-ADGroupMember -Identity $currentGroup -Members $currentUser -Confirm:$false
+            "$Time $fullName added to $class" | Out-File $ScriptsFilePath\groupAssignment.log -Append
+        } catch {
+            $ErrorMessage = $_.Exception.Message
+            $FailedItem = $_.Exception.ItemName
+            "$Time Problem adding $fullName to $class $ErrorMessage" | Out-File $ScriptsFilePath\groupAssignment.log -Append
+        }
+
+        try {
+            $currentCount = $currentUser.postalCode
+            $currentCountInt = [int]$currentCount
+            $newCount = $currentCountInt + 1
+            Set-ADUser -Identity $fullName -PostalCode $newCount
+            "$Time $fullName class count incremented" | Out-File $ScriptsFilePath\groupAssignment.log -Append
+        } catch {
+            $ErrorMessage = $_.Exception.Message
+            $FailedItem = $_.Exception.ItemName
+            "$Time Problem incrementing class count of $fullName $ErrorMessage" | Out-File $ScriptsFilePath\groupAssignment.log -Append
+        }
+    }
+    elseif ($status -eq "X" -or $status -eq "D")
+    {
+        try {
+            $currentUser = Get-ADUser -Identity $fullName -Properties postalCode
+            $currentGroup = Get-ADGroup -Identity $class
+            Remove-ADGroupMember -Identity $currentGroup -Members $currentUser -Confirm:$false
+            "$Time $fullName removed from $class" | Out-File $ScriptsFilePath\groupAssignment.log -Append
+        } catch {
+            $ErrorMessage = $_.Exception.Message
+            $FailedItem = $_.Exception.ItemName
+            "$Time Problem deleting $fullName from $class $ErrorMessage" | Out-File $ScriptsFilePath\groupAssignment.log -Append
+        }
+        try {
+            $currentCount = $currentUser.postalCode
+            $currentCountInt = [int]$currentCount
+            $newCount = $currentCountInt - 1
+            Set-ADUser -Identity $fullName -PostalCode $newCount
+            "$Time $fullName class count decremented" | Out-File $ScriptsFilePath\groupAssignment.log -Append
+        } catch {
+            $ErrorMessage = $_.Exception.Message
+            $FailedItem = $_.Exception.ItemName
+            "$Time Problem decrementing class count of $fullName $ErrorMessage" | Out-File $ScriptsFilePath\groupAssignment.log -Append
+        }
+    }
+}
+
+#final loop to remove users with 0 classes
+foreach ($line in $UniqueUserList)
+{
+
+    $FirstName = $line.firstname
+    $FirstNameLetter = $line.Firstname[0]
+    $LastName = $line.lastname
+    $SID = $line.'Student ID'
+
+    $fullName = $SID + $FirstNameLetter + $LastName
+     
+    $currentUser = Get-ADUser -Identity $fullName -Properties postalCode
+
+    $classCount = $currentUser.postalCode
+    $classCountInt = [int]$ClassCount
+
+    $HomeDirectoryLocation = $HomeDirectory + $fullName
+
+    if ($classCountInt -le 0)
+    {
+        try {
+            Remove-ADUser -Identity $fullName -Confirm:$false
+            Remove-Item -Recurse -Path $HomeDirectoryLocation -Confirm:$false
+            "$Time $fullName successfully deleted" | Out-File $ScriptsFilePath\userDeletion.log -Append
+        } catch {
+            $ErrorMessage = $_.Exception.Message
+            $FailedItem = $_.Exception.ItemName
+            "$Time  $ErrorMessage" | Out-File $ScriptsFilePath\userDeletion.log -Append
+        }
+    }
+
+}
---
+++ Var_Definition-1.ps1
@@ -0,0 +1,32 @@
+﻿# Name: 
+#
+#Modify the variables according to your system. Do not Add or remove any of the variables
+#
+#Set a variable for the location of the CSV file. For example, if CSV file
+#is located in C:\PowerShell
+$ScriptsFilePath = "C:\Scripts"
+#
+#Set a variable for the CSV file name. 
+$csvFileName = 'SmallSample.csv'
+#
+#Set a variable for the OU where user accounts will be stored
+#For example, "OU=Student Accounts,OU=MyExampleUniversity,DC=MyDomainExample,DC=local"
+$OUPathForAccounts = "OU=Student Accounts,OU=IoT University,DC=CharlieDolton,DC=local"
+#
+#Set a variable for the OU where user Groups will be stored
+#For example, "OU=Course Groups,OU=MyExampleUniversity,DC=CST412Project,DC=local"
+$OUPathForGroups = "OU=Course Groups,OU=IoT University,DC=CharlieDolton,DC=local"
+#
+#Set a variable to the location of users home directory
+$HomeDirectory = "\\server2019-1\sysvol\CharlieDolton.local\StudentsHomeDir\"
+#
+#Set a variable for your server name
+$ServerName = "Server2019-1"
+#
+#Set a variable for the shared folder name
+$FolderShare = "StudentsHomeDir"
+#
+#Set a variable for the domain Name
+$DomainName = "CharlieDolton.local"
+# 
+
